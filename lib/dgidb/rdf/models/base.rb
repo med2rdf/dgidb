ca2aa3314e26c6c67eafe76aef6e8765a24dcb46
Daisuke Satoh <daisuke.satoh@level-five.jp>
2017-12-24 23:01:15 +0900

    implement seed_if_needed

--- lib/dgidb/rdf/models/base.rb
+++ lib/dgidb/rdf/models/base.rb
@@ -22,6 +22,17 @@ module Dgidb
         self.conf = YAML.load_file(DB_YAML)[DB_CONF]
 
         establish_connection(conf)
+
+        def self.seed_if_needed
+          models = [Dgidb::RDF::Models::Drug,
+                    Dgidb::RDF::Models::Gene,
+                    Dgidb::RDF::Models::Interaction]
+
+          return unless models.map(&:count).map(&:zero?).all?
+
+          load File.join(Dgidb::RDF::ROOT_DIR, 'db', 'seed.rb')
+          Seed.execute
+        end
       end
     end
   end
